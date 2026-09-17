a96d2aead427fdf54e7689b6b757ee9c83cb88e0
leesoolyn02 <26730015@g.sewu.ac.kr>
2026-09-17 14:03:05 +0900

    Add files via upload

---
+++ 3. 행, 열, 문자 인수로 받아 중첩 반복문으로 출력.py
@@ -0,0 +1,15 @@
+'''def printPattern(rows=5, cols=5, char="*"):
+    for _ in range(rows):
+        for _ in range(cols):
+            print(char, end="")
+        print()
+
+printPattern(3, 10, "%")'''
+
+def printPattern(rows=5, cols=5, char="*"):
+    for _ in range(rows):
+        for _ in range(cols):
+            print(char, end="")
+        print()
+
+printPattern()
